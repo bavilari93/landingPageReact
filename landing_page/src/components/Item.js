ac33d5619fdf6d7e60019ec85f624694998b9cf2
Bianca A <frank@Bianca-Avila-4.local>
2018-04-29 20:04:41 +0900

    single display

--- landing_page/src/components/Item.js
+++ landing_page/src/components/Item.js
@@ -3,7 +3,7 @@ import React , {Component} from 'react'
 class Item extends Component{
 	render(){
 		return(
-			<div>
+			<div className="item">
 			<img src={this.props.url} alt=""/>
 			</div>)
 	}
